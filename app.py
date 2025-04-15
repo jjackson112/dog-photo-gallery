cd3b082e7ceffa8b2283a56b6635853a275769a9
Jazz <jjackson11211@gmail.com>
2025-04-14 23:30:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+
 import requests
 
 #imports a dictionary of data from dog_breeds.py and "prettifies", or styles, the dog names when they appear in the HTML page
@@ -11,25 +12,26 @@ app = Flask("app")
 def check_breed(breed):
   return "/".join(breed.split("-"))
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def dog_image_gallery():
-# empty list to handle errors, will hold error message
-  errors = []
-# request.form.get collects values with GET
+# add error handling to the app, will hold error message
+  errors=[]
+# save data from request.method, add breed variable
   if request.method == "POST":
     breed = request.form.get("breed")
     number = request.form.get("number")
     if not breed:
-      errors.append("Oops! Please choose a breed.")
+      errors.append("Oops! Please select a breed.")
     if not number:
-      errors.append("Oops! Please choose a number.")
+      errors.append("Oops! Please select a number.")
     if breed and number:
-# concatenating the check_breed(breed) will place the breed name after the last /
-# concatenating the URL parameter pulls 30 random images of selected breed
       response = requests.get("https://dog.ceo/api/breed/" + check_breed(breed) + "/images/random/" + number)
       data = response.json()
-      dog_images = data["message"]
-# images holds the dictionary data, breed holds the dog breed name from POST request, and then the errors list
+      dog_images = data["messages"]
       return render_template("dogs.html", images=dog_images, breed=prettify_dog_breed(breed), errors=[])
-# the empty list and blank string will handle the HTML template when no selection has been made, errors will display the error message
+  # this return will handle the HTML template when no dropmenu selection has been made
   return render_template("dogs.html", images=[], breed="", errors=errors)
+
+app.debug = True
+# Run the flask server
+app.run(host='0.0.0.0', port=8080)
